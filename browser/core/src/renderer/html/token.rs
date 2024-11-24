03579b5ff9f8251edb70fa0ba9afc7d39258991c
sobatha <natsuki.yokomizo.ny@gmail.com>
2024-11-24 10:02:30 +0900

    Add necessary types

---
+++ browser/core/src/renderer/html/token.rs
@@ -0,0 +1,80 @@
+use alloc::string::String;
+use alloc::vec::Vec;
+use crate::renderer::html::attribute::Attribute;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct HtmlTokenizer {
+    state: State,
+    pos: usize,
+    reconsume: bool,
+    latest_token: Option<HtmlToken>,
+    input: Vec<char>,
+    buf: String,
+}
+
+impl HtmlTokenizer {
+    pub fn new(html: String) -> Self {
+        Self {
+            state: State::Data,
+            pos: 0,
+            reconsume: false,
+            latest_token: None,
+            input: html.chars().collect(),
+            buf: String::new(),
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum HtmlToken {
+    StartTag {
+        tag: String,
+        self_closing: bool,
+        attributes: Vec<Attribute>,
+    },
+    EndTag {
+        tag: String,
+    },
+    Char(char),
+    Eof,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum State {
+    /// https://html.spec.whatwg.org/multipage/parsing.html#data-state
+    Data,
+    /// https://html.spec.whatwg.org/multipage/parsing.html#tag-open-state
+    TagOpen,
+    /// https://html.spec.whatwg.org/multipage/parsing.html#end-tag-open-state
+    EndTagOpen,
+    /// https://html.spec.whatwg.org/multipage/parsing.html#tag-name-state
+    TagName,
+    /// https://html.spec.whatwg.org/multipage/parsing.html#before-attribute-name-state
+    BeforeAttributeName,
+    /// https://html.spec.whatwg.org/multipage/parsing.html#attribute-name-state
+    AttributeName,
+    /// https://html.spec.whatwg.org/multipage/parsing.html#after-attribute-name-state
+    AfterAttributeName,
+    /// https://html.spec.whatwg.org/multipage/parsing.html#before-attribute-value-state
+    BeforeAttributeValue,
+    /// https://html.spec.whatwg.org/multipage/parsing.html#attribute-value-(double-quoted)-state
+    AttributeValueDoubleQuoted,
+    /// https://html.spec.whatwg.org/multipage/parsing.html#attribute-value-(single-quoted)-state
+    AttributeValueSingleQuoted,
+    /// https://html.spec.whatwg.org/multipage/parsing.html#attribute-value-(unquoted)-state
+    AttributeValueUnquoted,
+    /// https://html.spec.whatwg.org/multipage/parsing.html#after-attribute-value-(quoted)-state
+    AfterAttributeValueQuoted,
+    /// https://html.spec.whatwg.org/multipage/parsing.html#self-closing-start-tag-state
+    SelfClosingStartTag,
+    /// https://html.spec.whatwg.org/multipage/parsing.html#script-data-state
+    ScriptData,
+    /// https://html.spec.whatwg.org/multipage/parsing.html#script-data-less-than-sign-state
+    ScriptDataLessThanSign,
+    /// https://html.spec.whatwg.org/multipage/parsing.html#script-data-end-tag-open-state
+    ScriptDataEndTagOpen,
+    /// https://html.spec.whatwg.org/multipage/parsing.html#script-data-end-tag-name-state
+    ScriptDataEndTagName,
+    /// https://html.spec.whatwg.org/multipage/parsing.html#temporary-buffer
+    TemporaryBuffer,
+}
